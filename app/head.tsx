98b44f9efe7c3f5764f22e01da3671ac7977c72f
Gustavo Peña <gustavo.pema@gmail.com>
2023-02-06 20:12:28 -0600

    feat: Add social meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,12 @@ export default function Head() {
       <title>Frontend Dev | Gustavo</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Frontend developer" />
+      {/* Social tags */}
+      <meta property="og:title" content="Frontend Dev | Gustavo Peña" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://gustavopena.dev" />
+      <meta property="og:image" content="/sample.png" />
+      {/* favicon */}
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
